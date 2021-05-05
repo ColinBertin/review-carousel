fd5c623ace4b7d96ca7f8c9b9d300ffa265ece7f
Colin BE <cayno334@hotmail.com>
2021-05-05 10:39:04 +0900

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const profils = [
+    {
+        id: "1";
+        name: "John";
+        profil: "Web Developer";
+        img: "./img/john.jpeg";
+        intro: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lobortis erat, sed ultricies nulla. Ut ac hendrerit lorem. Nulla eget est ut magna suscipit pretium a vel nisl. Phasellus in ipsum dapibus, egestas metus a, elementum quam. Etiam est sapien, dapibus nec augue eget, imperdiet venenatis sem. Nam ante dolor, sodales vitae pulvinar ac, dictum ac urna. Morbi sit amet leo sed velit rutrum semper. Curabitur pellentesque sapien et sem eleifend pellentesque. Duis risus est, gravida eget tortor vel, laoreet euismod metus. Phasellus nec est eu libero ultricies molestie.";
+    },
+    {
+        id: "1";
+        name : "";
+        profil : "";
+        intro : "Donec a auctor mi, in hendrerit metus. Pellentesque a porta metus, eget ultricies odio. Nunc fringilla massa id massa eleifend pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam lectus lectus, interdum eu est ut, mattis dictum orci. Morbi nec nulla ligula. Sed tempor, elit sed luctus pulvinar, nisl sem blandit dui, ut dignissim nunc nulla eget nisi. Vestibulum varius nulla gravida, congue leo quis, finibus ex. Maecenas placerat volutpat sem, at varius tellus condimentum ac. Phasellus ipsum velit, vulputate vitae porta vitae, vehicula id purus. Maecenas rutrum ligula quis gravida blandit. Morbi viverra ac dolor a consequat. Pellentesque pellentesque lectus orci.";
+    },
+    {
+        id: "1";
+        name : "";
+        profil : "";
+        intro : "Phasellus ipsum nisi, dignissim ac efficitur sed, porttitor a leo. Donec imperdiet odio nulla, eu fringilla turpis congue vitae. Etiam ligula augue, fringilla id mollis quis, pretium in est. Sed porta accumsan ligula, sit amet consequat tellus tristique ac. Nullam nulla lorem, volutpat quis enim quis, vehicula gravida est. Fusce eu iaculis urna. Praesent laoreet luctus odio bibendum consequat. Nullam efficitur mauris non dolor congue, nec feugiat odio fermentum. Suspendisse sit amet aliquet leo.";
+    },
+    {
+        id: "1";
+        name : "";
+        profil : "";
+        intro : "Integer convallis dui orci, sed sodales orci dignissim sed. Pellentesque at quam id lorem dignissim sollicitudin et non nulla. Sed dignissim rhoncus enim, at egestas massa volutpat vitae. Cras pulvinar, risus a bibendum dictum, nunc neque gravida ex, eu semper metus urna a velit. Donec et quam magna. Nulla quis ipsum efficitur, euismod massa non, imperdiet augue. Donec et interdum magna, vel vehicula massa. In ac sem libero. Aliquam vel neque id justo rutrum porttitor tempor quis erat. Donec scelerisque elementum congue. Praesent congue quis elit at faucibus. Nam ac ullamcorper erat. Sed finibus nec risus at commodo. Mauris quam metus, malesuada a nunc sed, luctus tristique ipsum. Etiam volutpat dictum arcu sit amet semper.";
+    },
+    {
+        id: "1";
+        name : "";
+        profil : "";
+        intro : "Quisque mollis, turpis nec posuere suscipit, lorem arcu lobortis dui, ut tincidunt nisi odio a massa. Duis luctus mollis turpis, sed dignissim nisl laoreet ac. Vivamus viverra placerat leo, ac facilisis ante malesuada et. Ut quis erat leo. Integer tellus quam, finibus eget velit ac, interdum hendrerit purus. Proin at tempor neque. Praesent condimentum felis dolor, at ornare purus pulvinar eget. Vestibulum pretium vestibulum erat ut volutpat. Vivamus ut justo vel sem tristique lobortis at eu arcu. Vivamus facilisis varius tincidunt. Aliquam at luctus massa, eget tincidunt nisi. Quisque nunc nibh, sollicitudin nec consequat ut, sollicitudin nec ex. Proin in bibendum nulla. Aliquam ipsum massa, pretium sed tortor at, aliquam semper magna. Ut nisi mi, congue eget velit nec, dignissim vulputate dui. Proin eget rhoncus ex, et faucibus dolor.";
+    }
+]
